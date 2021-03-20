ebf682ccccc89d3d78be26df9d6579422319471f
FacundoFalcioni2410 <facundofalcioni2410@gmail.com>
2021-03-20 20:34:44 -0300

    ejercicios

--- ejercicio11/app.ts
+++ ejercicio11/app.ts
@@ -1,5 +1,16 @@
 const isPalindrome = (string: string) =>{
-    return string.replace(/\s/g,'').toLowerCase().split('').reverse().join('') === string.replace(/\s/g,'').toLowerCase();
+    let arr: string[] = [];
+    let buffer: string;
+
+    string = string.replace(/\s/g,'').replace(',','').replace('.','').replace('\'','');
+    string = string.toLowerCase();
+    arr = string.split('');
+    arr = arr.reverse();
+    buffer = arr.join('');
+    
+    return string === buffer;
+    
+    //return string.replace(/\s/g,'').toLowerCase().split('').reverse().join('') === string.replace(/\s/g,'').toLowerCase();
 }
 
-console.log(isPalindrome("level"));
+console.log(isPalindrome("Madam, I'm Adam"));
---
+++ ejercicio12/app.ts
@@ -0,0 +1,153 @@
+const analizarFecha = (cadena: string) :string =>{
+    let arr: string[] = [];
+    let mes: number;
+    let dia: number;
+
+    arr = cadena.split('/');
+    mes = parseInt(arr[1]);
+    dia = parseInt(arr[0]);
+    return signoPorFecha(dia,mes);
+
+}
+
+const signoPorFecha = (dia: number, mes: number) =>{
+    let respuesta: string = "";
+    Math.abs(mes);
+    switch(mes)
+    {          
+        case 3:
+            if(dia < 21)
+            {
+                respuesta = "Piscis";
+            }
+            else
+            {
+                respuesta = "Aries";
+            }
+            break;
+        case 4:
+            if(dia < 21)
+            {
+                respuesta = "Aries";
+            }
+            else
+            {
+                respuesta = "Tauro";
+            }
+            break;
+        case 5:
+            if(dia < 22)
+            {
+                respuesta = "Tauro";
+            }
+            else
+            {
+                respuesta = "Geminis";
+            }
+            break;
+        case 6:
+            if(dia < 23)
+            {
+                respuesta = "Geminis";
+            }
+            else
+            {
+                respuesta = "Cancer";
+            }
+            break;
+        case 7:
+            if(dia < 24)
+            {
+                respuesta = "Cancer";
+            }
+            else
+            {
+                respuesta = "Leo";
+            }
+            break;
+        case 8:
+            if(dia < 23)
+            {
+                respuesta = "Leo";
+            }
+            else
+            {
+                respuesta = "Virgo";
+            }
+            break;
+        case 8:
+            if(dia < 23)
+            {
+                respuesta = "Leo";
+            }
+            else
+            {
+                respuesta = "Virgo";
+            }
+            break;
+        case 9:
+            if(dia < 23)
+            {
+                respuesta = "Virgo";
+            }
+            else
+            {
+                respuesta = "Libra";
+            }
+            break;
+        case 10:
+            if(dia < 23)
+            {
+                respuesta = "Libra";
+            }
+            else
+            {
+                respuesta = "Escorpio";
+            }
+            break;
+        case 11:
+            if(dia < 23)
+            {
+                respuesta = "Escorpio";
+            }
+            else
+            {
+                respuesta = "Sagitario";
+            }
+            break;
+        case 12:
+            if(dia < 22)
+            {
+                respuesta = "Sagitario";
+            }
+            else
+            {
+                respuesta = "Capricornio";
+            }
+            break;
+        case 1:
+            if(dia < 21)
+            {
+                respuesta = "Capricornio";
+            }
+            else
+            {
+                respuesta = "Acuario";
+            }
+            break;
+        case 1:
+            if(dia < 19)
+            {
+                respuesta = "Acuario";
+            }
+            else
+            {
+                respuesta = "Piscis";
+            }
+            break;
+    }
+
+    return respuesta;
+}
+
+console.log(analizarFecha("24/10/2000"));
