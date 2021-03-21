e9e63484e000dc6f7b3056be3da1369ac9d4326d
FacundoFalcioni2410 <facundofalcioni2410@gmail.com>
2021-03-21 17:08:40 -0300

    correccion ejercicio13

--- clase1/ejercicio13/app.js
+++ clase1/ejercicio13/app.js
@@ -1,16 +1,14 @@
 "use strict";
-var numerosPrimos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+var numerosPrimos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199];
 var descomponerNumero = function (numero) {
-    var result = [];
-    for (var i = 0; numero != 1; i++) {
+    var factoresPrimos = [];
+    for (var i = 0; i < numerosPrimos.length && numero != 1; i++) {
         while (numero % numerosPrimos[i] === 0) {
-            if (numero % numerosPrimos[i] === 0) {
-                numero = numero / numerosPrimos[i];
-                result.push(numerosPrimos[i]);
-            }
+            numero = numero / numerosPrimos[i];
+            factoresPrimos.push(numerosPrimos[i]);
         }
     }
-    var string = result.join('');
+    var string = factoresPrimos.join('');
     return sumarTerminos(parseInt(string));
 };
 var iguales = function (numero1, numero2) {
@@ -25,5 +23,5 @@ var sumarTerminos = function (numero) {
     }
     return result;
 };
-console.log(iguales(sumarTerminos(378), descomponerNumero(378)));
+console.log(iguales(sumarTerminos(1086), descomponerNumero(1086)));
 //# sourceMappingURL=app.js.map
--- clase1/ejercicio13/app.ts
+++ clase1/ejercicio13/app.ts
@@ -1,21 +1,18 @@
-const numerosPrimos: number[] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+const numerosPrimos: number[] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199];
 
 const descomponerNumero = (numero: number) =>{
-    let result: number[] = [];
+    let factoresPrimos: number[] = [];
 
-    for(let i: number = 0; numero != 1; i++)
+    for(let i: number = 0; i < numerosPrimos.length && numero != 1; i++)
     {
         while(numero % numerosPrimos[i] === 0)
-        {
-            if(numero % numerosPrimos[i] === 0)
-            {   
-                numero = numero / numerosPrimos[i];
-                result.push(numerosPrimos[i]);
-            }
+        {   
+            numero = numero / numerosPrimos[i];
+            factoresPrimos.push(numerosPrimos[i]);
         }
     }
 
-    let string: string = result.join('');
+    let string: string = factoresPrimos.join('');
     return sumarTerminos(parseInt(string));
 }
 
@@ -35,6 +32,6 @@ const sumarTerminos = (numero: number) =>{
     return result;   
 }
 
-console.log(iguales(sumarTerminos(378), descomponerNumero(378)));
+console.log(iguales(sumarTerminos(1086), descomponerNumero(1086)));
 
 
